70e5ec863763501a6cd026934e8eb46b268d8087
goddl717 <37294326+goddl717@users.noreply.github.com>
2018-12-21 17:04:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dnwls/moapp_project_sing/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dnwls.moapp_project_sing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // 테스트용
+    //테스트용 2
+}
